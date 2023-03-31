caf664a5f6342f1d991140c7983c551a68f04a57
cristianorneto@gmail.com <cristianorneto@gmail.com>
2023-03-31 12:03:12 -0300

    Pnia Store and Function

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import SearchView from '../views/SearchView.vue'
+import LibraryView from '../views/LibraryView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
       component: HomeView
+    },
+    {
+      path: '/search',
+      component: SearchView
+    },
+    {
+      path: '/library',
+      component: LibraryView
     },
   ]
 })
